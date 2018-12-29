c801baffe98ceb69979f1040fad30ce7cbe778ba
Paco Brevers <francisco.brevers@tecnilogica.com>
2018-12-29 19:09:10 +0100

    Gestion de errores personalizados.

---
+++ common/api_errors.py
@@ -0,0 +1,14 @@
+""" Custom errors"""
+
+errors = {
+    'ErrorA': {
+        'message': "Mensaje",
+        'status': 400,
+        'extra': 'Opcional'
+    },
+    'ErrorB': {
+        'message': "Mensaje.",
+        'status': 410,
+        'extra': "Opcional.",
+    },
+}
--- polcheck-app.py
+++ polcheck-app.py
@@ -1,13 +1,27 @@
 """ Flask server """
 from flask import Flask
+from flask import got_request_exception
 from flask_restful import Api
+
+# Resources
 from resources.politico import PoliticoAPI
 from resources.politicos import PoliticosAPI
 from resources.csv_import import CSVImportAPI
 
+# Config
+import common.api_errors
+
 # Flask singleton
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, catch_all_404s=True, errors=common.api_errors)
+
+
+def log_exception(sender, exception, **extra):
+    """ Log an exception to our logging framework """
+    sender.logger.debug('Got exception during processing: %s', exception)
+
+
+got_request_exception.connect(log_exception, app)
 
 # Routes
 api.add_resource(CSVImportAPI, '/import', '/polcheck/import', endpoint='import')
